f72c11ff37a9e16dfa74cc0d01c8118fb09227ca
Ayose <ayose@foton.es>
2010-08-27 14:13:15 +0100

    Sanitize HTML output for static pages

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -13,4 +13,74 @@ module StaticPagesHelper
     end
   end
 
+  def sanitize_static_content(content)
+    tokenizer = HTML::Tokenizer.new(content)
+    output = ''
+    while token = tokenizer.next
+
+      if token[0,1] != "<"
+        output << escape_once(token)
+        next
+      end
+
+      # Extract HTML tag and attributes
+      html_tag = nil
+      attributes = {}
+      if token =~ /\A<(\/?\w+)(?:\s+(.*))?\s*>\Z/
+        html_tag = $1
+        if $2
+          $2.scan(/(\w+)\s*=["'](.*?)["']/).each {|name, value| attributes[name] = value }
+        end
+      end
+
+      case html_tag.downcase
+      when "img"
+        output << %[<img src="#{escape_once attributes["src"]}" alt="" />]
+
+      when "a"
+        output << %[<a href="#{escape_once attributes["href"]}" rel="nofollow">]
+
+      when "br", "hr"
+        output << "<#{html_tag} />"
+
+      when "b", "strong"
+        output << "<strong>"
+
+      when "/b", "/strong"
+        output << "</strong>"
+
+      when "i", "em"
+        output << "<em>"
+
+      when "/i", "/em"
+        output << "</em>"
+
+      when "u"
+        output << %[<span style="text-decoration: underline;">]
+
+      when "div", "p"
+        added_attr = ''
+        if align = attributes["align"] and %w(right center left).include?(align)
+          added_attr = " style=\"text-align: #{align}\""
+        end
+        output << "<p#{added_attr}>"
+
+      when "div", "p"
+        output << "</p>"
+
+      when "s", "strike"
+        output << %[<span style="text-decoration: line-through;">]
+
+      when "/s", "/strike", "/u"
+        output << %[</span>]
+
+      when "/a", "ul", "/ul", "ol", "/ol", "li", "/li", "h2", "/h2", "h3", "/h3"
+        output << "<#{html_tag}>"
+
+      end
+
+    end
+    output.html_safe
+  end
+
 end
