075b45899f2351828ea0f094a7b064798f8336cd
Ayose <ayose@foton.es>
2010-09-15 14:55:49 +0100

    Added tables to the valid tags, without attributes

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -13,6 +13,8 @@ module StaticPagesHelper
     end
   end
 
+  ValidTagsWithoutAttributes = %w(a ul ol li h2 h3 table thead tfoot tr th td)
+
   def sanitize_static_content(content)
     tokenizer = HTML::Tokenizer.new(content)
     output = ''
@@ -33,53 +35,53 @@ module StaticPagesHelper
         end
       end
 
-      if not html_tag.nil?
-        case html_tag.downcase
-          when "img"
-            output << %[<img src="#{escape_once attributes["src"]}" alt="" />]
+      case html_tag.to_s.downcase
+      when "img"
+        output << %[<img src="#{escape_once attributes["src"]}" alt="" />]
 
-          when "a"
-            output << %[<a href="#{escape_once attributes["href"]}" rel="nofollow">]
+      when "a"
+        output << %[<a href="#{escape_once attributes["href"]}" rel="nofollow">]
 
-          when "br", "hr"
-            output << "<#{html_tag} />"
+      when "br", "hr"
+        output << "<#{html_tag} />"
 
-          when "b", "strong"
-            output << "<strong>"
+      when "b", "strong"
+        output << "<strong>"
 
-          when "/b", "/strong"
-            output << "</strong>"
+      when "/b", "/strong"
+        output << "</strong>"
 
-          when "i", "em"
-            output << "<em>"
+      when "i", "em"
+        output << "<em>"
 
-          when "/i", "/em"
-            output << "</em>"
+      when "/i", "/em"
+        output << "</em>"
 
-          when "u"
-            output << %[<span style="text-decoration: underline;">]
+      when "u"
+        output << %[<span style="text-decoration: underline;">]
 
-          when "div", "p"
-            added_attr = ''
-            if align = attributes["align"] and %w(right center left).include?(align)
-              added_attr = " style=\"text-align: #{align}\""
-            end
-            output << "<p#{added_attr}>"
-
-          when "div", "p"
-            output << "</p>"
-
-          when "s", "strike"
-            output << %[<span style="text-decoration: line-through;">]
-
-          when "/s", "/strike", "/u"
-            output << %[</span>]
-
-          when "/a", "ul", "/ul", "ol", "/ol", "li", "/li", "h2", "/h2", "h3", "/h3"
-            output << "<#{html_tag}>"
-
-          end
+      when "div", "p"
+        added_attr = ''
+        if align = attributes["align"] and %w(right center left).include?(align)
+          added_attr = " style=\"text-align: #{align}\""
         end
+        output << "<p#{added_attr}>"
+
+      when "div", "p"
+        output << "</p>"
+
+      when "s", "strike"
+        output << %[<span style="text-decoration: line-through;">]
+
+      when "/s", "/strike", "/u"
+        output << %[</span>]
+
+      else
+        if ValidTagsWithoutAttributes.include?(html_tag.sub(/^\//, ''))
+          output << "<#{html_tag}>"
+        end
+
+      end
 
     end
     output.html_safe
